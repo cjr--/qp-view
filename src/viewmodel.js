641f5d22068f55f7071b9a6d8a379912e6e5aa0d
Chris Richardson <cjr@hotmail.co.uk>
2017-02-23 20:48:18 +0000

    viewmodel - controls collection

--- src/viewmodel.js
+++ src/viewmodel.js
@@ -9,6 +9,8 @@ define(module, function(exports, require, make) {
 
     view: null,
 
+    controls: [],
+
     init: function(o) {
       o = qp.options(o, { bind: true, draw: true });
       this.view = view.create({
@@ -24,7 +26,41 @@ define(module, function(exports, require, make) {
     },
 
     read: function(node) {
+      // should this return an independant tree?
+      // one-way binding but still able to get control values
+      // one.5-way binding?
+      // manual binding?
+      // v-on-blur="handler"
+      // v-on-change="handler"
+      // sugar for manual binding;
+      // v-on-change="path" - if path is a property it sets the property
+    },
 
+    // <div v-control="control_1"> ... </div>
+    //
+    // add value to the object config?
+    //
+    // GETTERS
+    // this.control('control_1')
+    // this.control([ 'control_1', 'control_2' ])
+    //
+    // SETTERS
+    // this.control('control_1', { key: value, ... })
+    // this.control([ 'control_1', 'control_2' ], { key: value, ... })
+    // this.control({ id: 'control_1', key: value, ... })
+    // this.control([{ id: 'control_1', key: value, ... }, { id: 'control_2', key: value, ... }])
+    control: function(arg0, arg1) {
+      if (qp.is(arg0, 'string')) {
+        if (qp.is(arg1, 'object')) {
+
+        } else {
+          return qp.find(this.controls, { id: arg0 });
+        }
+      } else if (qp.is(arg0, 'array')) {
+
+      } else if (qp.is(arg0, 'object')) {
+
+      }
     }
 
   });
