dae852101886d0b47afd7bd7422dfc7a0aac6db2
Chris Richardson <cjr@hotmail.co.uk>
2017-03-31 20:09:16 +0100

    build

--- dist/qp-view.js
+++ dist/qp-view.js
@@ -309,15 +309,8 @@ define(module, function(exports, require, make) {
 
     ns: 'qp-view/controller',
 
-    name: 'controller',
-
     views: [],
-
     history: [],
-
-    init: function(o) {
-      this.store = store.create({ key: 'help.littleacorns', ctx: o.state_context });
-    },
 
     add_view: function(o) {
       this.views.push({ key: o.key, ctor: o.ctor, el: o.el });
@@ -455,13 +448,10 @@ define(module, function(exports, require, make) {
     ns: 'qp-view/viewmodel',
 
     view: null,
-
     controller: null,
-
     visible: false,
     item_key: '',
     list_key: '',
-
     controls: [],
 
     init: function(o) {
@@ -476,6 +466,7 @@ define(module, function(exports, require, make) {
     },
 
     setup: function() { },
+
     load: function(data, done) { done(); },
     get_data: function(done) { },
     show: function() { },
@@ -492,13 +483,8 @@ define(module, function(exports, require, make) {
       }
     },
 
-    get_current_view: function() { return this.controller.get_current_view(); },
-    toggle_view: function(o) { this.controller.toggle_view(o); },
-    swap_view: function(o) { this.controller.swap_view(o); },
-    next_view: function(o) { this.controller.next_view(o); },
-    back_view: function(o) { this.controller.back_view(o); },
-    show_view: function(o) { this.controller.show_view(o); },
-    hide_view: function(o) { this.controller.hide_view(o); },
+    next: function(o) { this.controller.next_view(o); },
+    back: function(o) { this.controller.back_view(o); },
 
     set_visible: function(visible) {
       this.visible = visible;
@@ -511,42 +497,12 @@ define(module, function(exports, require, make) {
 
     read: function(node) {
       this.view.update_model(node);
-
-      // should this return an independant tree?
-      // one-way binding but still able to get control values
-      // one.5-way binding?
-      // manual binding?
-      // v-on-blur="handler"
-      // v-on-change="handler"
-      // sugar for manual binding;
-      // v-on-change="path" - if path is a property it sets the property
     },
 
-    // <div v-control="control_1"> ... </div>
-    //
-    // add value to the object config?
-    //
-    // GETTERS
-    // this.control('control_1')
-    // this.control([ 'control_1', 'control_2' ])
-    //
-    // SETTERS
-    // this.control('control_1', { key: value, ... })
-    // this.control([ 'control_1', 'control_2' ], { key: value, ... })
-    // this.control({ id: 'control_1', key: value, ... })
-    // this.control([{ id: 'control_1', key: value, ... }, { id: 'control_2', key: value, ... }])
-    control: function(arg0, arg1) {
-      if (qp.is(arg0, 'string')) {
-        if (qp.is(arg1, 'object')) {
-
-        } else {
-          return qp.find(this.controls, { id: arg0 });
-        }
-      } else if (qp.is(arg0, 'array')) {
-
-      } else if (qp.is(arg0, 'object')) {
-
-      }
+    set_data: function(data, done) {
+      qp.merge(this, data);
+      this.draw();
+      qp.invoke_next(done);
     }
 
   });
